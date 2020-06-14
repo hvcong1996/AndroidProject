63e9aba1ad1089bb1874f3e50b11c2bde733624e
hvcong <hvcong@tma.com.vn>
2020-06-14 15:05:30 +0700

    Update code for ConstraintLayout

--- app/src/main/java/com/example/myproject/MainActivity.kt
+++ app/src/main/java/com/example/myproject/MainActivity.kt
@@ -1,12 +1,69 @@
 package com.example.myproject
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    private  fun setListeners(){
+        val boxOneText = findViewById<TextView>(R.id.box_one_text)
+        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
+        val boxFourText = findViewById<TextView>(R.id.box_four_text)
+        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+
+        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+//        val clickableViews: List<View> = listOf(boxOneText,boxTwoText,boxThreeText,boxFourText,boxFiveText,rootConstraintLayout)
+//
+//        for(item in clickableViews){
+//            item.setOnClickListener{
+//                makeColored(it)
+//            }
+//        }
+
+        val redButton = findViewById<Button>(R.id.red_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val clickableButtonViews: List<View> = listOf(boxOneText,boxTwoText,boxThreeText,boxFourText,boxFiveText,rootConstraintLayout,redButton,yellowButton,greenButton)
+
+
+        for(item in clickableButtonViews){
+            item.setOnClickListener{
+                makeColored(it)
+            }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when(view.id){
+            // Note: Có thể sử dụng drawable để set imge như bên dưới
+            //R.id.box_one_text -> view.setBackgroundColor(R.drawable.imge)
+
+
+            R.id.box_one_text -> view.setBackgroundColor(Color.RED)
+            R.id.box_two_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_three_text -> view.setBackgroundColor(Color.GREEN)
+            R.id.box_four_text -> view.setBackgroundColor(Color.YELLOW)
+            R.id.box_five_text -> view.setBackgroundColor(Color.DKGRAY)
+
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }

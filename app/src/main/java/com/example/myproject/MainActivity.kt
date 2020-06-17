91b527f471dd4300b1b9ef630b4099b33180ae5a
hvcong <hvcong@tma.com.vn>
2020-06-17 22:04:59 +0700

    Update process for binding and binding data class

--- app/src/main/java/com/example/myproject/MainActivity.kt
+++ app/src/main/java/com/example/myproject/MainActivity.kt
@@ -5,63 +5,63 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.myproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Aleks Haecky")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.btn_done).setOnClickListener {
+        binding.myName = this.myName
+
+        binding.btnDone.setOnClickListener{
             addNickName(it)
         }
 
-        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+        binding.nicknameText.setOnClickListener {
             updateNickname(it)
         }
     }
 
     // handler button click case 2
     private fun addNickName(view: View){
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE // GONE text input
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            //invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            nicknameText.visibility = View.GONE
+        }
         view.visibility = View.GONE // GONE item truyen vao, item o day la button
-        nicknameTextView.visibility = View.VISIBLE // VISIBLE textView nickname
 
         // Hide the keyboard.
-//        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     private fun updateNickname (view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val doneButton = findViewById<Button>(R.id.btn_done)
-
-        editText.visibility = View.VISIBLE
-        doneButton.visibility = View.VISIBLE
+        binding.apply {
+            nicknameEdit.visibility = View.VISIBLE
+            btnDone.visibility = View.VISIBLE
+            // Set the focus to the edit text.
+            nicknameEdit.requestFocus()
+        }
         view.visibility = View.GONE
 
-        // Set the focus to the edit text.
-        editText.requestFocus()
         // Show the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(editText, 0)
+        imm.showSoftInput(binding.nicknameEdit, 0)
     }
 
     // handler button click case 1
     private fun clickHandlerFunction(viewThatIsClicked: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE // GONE text input
+        binding.nicknameText.text = binding.nicknameEdit.text
+        binding.nicknameEdit.visibility = View.GONE // GONE text input
         viewThatIsClicked.visibility = View.GONE // GONE item truyen vao, item o day la button
-        nicknameTextView.visibility = View.VISIBLE // VISIBLE textView nickname
+        binding.nicknameText.visibility = View.VISIBLE // VISIBLE textView nickname
     }
 }
